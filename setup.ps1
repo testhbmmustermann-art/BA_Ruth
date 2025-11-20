d28ae027014a58330cdaeb6b3ad1e2f614206d42
testhbmmustermann-art <test.hbm.mustermann@gmail.com>
2025-11-20 19:59:18 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,13 @@
-Write-Host "=== Stopping existing n8n containers ==="
-docker ps -a --filter "name=n8n" --format "{{.ID}}" | ForEach-Object { docker stop $_; docker rm $_ }
+Write-Host "=== Stopping and removing existing n8n containers ==="
+docker ps -a --filter "name=n8n" --format "{{.ID}}" | ForEach-Object { 
+    docker stop $_
+    docker rm $_
+}
 
-Write-Host "=== Ensuring n8n_data volume exists ==="
+Write-Host "=== Removing old n8n_data volume (to allow NEW account creation) ==="
+docker volume rm n8n_data -f | Out-Null
+
+Write-Host "=== Creating fresh n8n_data volume ==="
 docker volume create n8n_data | Out-Null
 
 Write-Host "=== Starting new n8n container ==="
